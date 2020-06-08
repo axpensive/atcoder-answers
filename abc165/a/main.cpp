55c2a357a47a0caa2427221a3e697b61d65e6d24
axpensive <35427211+axpensive@users.noreply.github.com>
2020-06-08 22:58:06 +0900

    add: abc165

---
+++ abc165/a/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265359
+using namespace std;
+template <class T>
+inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+const long long INF = 1LL << 60;
+using pll = pair<long long, long long>;
+
+int main() {
+  // 標準入力が早くなる。Cスタイルの入出力とC++スタイルの入出力を混合させてはならない。
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  int k, a, b;
+
+  cin >> k >> a >> b;
+
+  for (int i = a; i <= b; i++) {
+    if (i % k == 0) {
+      cout << "OK" << endl;
+      return 0;
+    }
+  }
+
+  cout << "NG" << endl;
+
+  return 0;
+}
---
+++ abc165/b/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define PI 3.14159265359
+using namespace std;
+template <class T>
+inline bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <class T>
+inline bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+const long long INF = 1LL << 60;
+using pll = pair<long long, long long>;
+
+int main() {
+  // 標準入力が早くなる。Cスタイルの入出力とC++スタイルの入出力を混合させてはならない。
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  long long x;
+  cin >> x;
+  long long res = 0;
+
+  for (long long i = 100; i < x;) {
+    i += i / 100;
+    ++res;
+  }
+
+  cout << res << endl;
+
+  return 0;
+}

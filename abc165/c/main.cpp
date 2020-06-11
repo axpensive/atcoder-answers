b9b05b162717dfb9cca9913ade6e9168fbc7313e
axpensive <35427211+axpensive@users.noreply.github.com>
2020-06-11 22:57:07 +0900

    add: abc165 c

--- abc165/c/main.cpp
+++ abc165/c/main.cpp
@@ -1,30 +1,49 @@
 #include <bits/stdc++.h>
-#define PI 3.14159265359
 using namespace std;
-template <class T>
-inline bool chmax(T &a, T b) {
-  if (a < b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-template <class T>
-inline bool chmin(T &a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-const long long INF = 1LL << 60;
-using pll = pair<long long, long long>;
 
+int n, m, q;
+vector<long long> a, b, c, d;
+long long answer = 0;
+
+long long ans(vector<int> A) {
+  long long res = 0;
+  for (int i = 0; i < q; i++) {
+    if (A[b[i]] - A[a[i]] == c[i]) {
+      res += d[i];
+    }
+  }
+  return res;
+}
+
+long long dfs(vector<int> &A) {
+  if ((int)A.size() == n) {
+    ans(A);
+    return ans(A);
+  }
+  long long res = 0;
+  int pre_value = (A.empty() ? 0 : A.back());
+
+  for (int i = pre_value; i < m; i++) {
+    A.push_back(i);
+    res = max(res, dfs(A));
+    A.pop_back();
+  }
+  return res;
+}
 int main() {
-  // 標準入力が早くなる。Cスタイルの入出力とC++スタイルの入出力を混合させてはならない。
-  cin.tie(nullptr);
-  ios_base::sync_with_stdio(false);
+  cin >> n >> m >> q;
+  a.resize(q);
+  b.resize(q);
+  c.resize(q);
+  d.resize(q);
+  for (int i = 0; i < q; i++) {
+    cin >> a[i] >> b[i] >> c[i] >> d[i];
+    --a[i];
+    --b[i];
+  }
+  vector<int> A;
 
-  
+  cout << dfs(A) << endl;
+
   return 0;
 }
